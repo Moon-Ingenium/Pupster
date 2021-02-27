f740159d12f2ecc8ee15db9bdcc0b5109236fc9f
Michelle Moon <robbins.michelle@ymail.com>
2021-02-27 11:14:32 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDom from 'react-dom';
+import About from "./pages/About";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+      <Nav/>
+<Route exact path = "/" component = {About}/>
+<Route exact path = "/about" component = {About}/>
+<Route exact path = "/search" component = {Search}/>
+<Route exact path = "/discover" component = {Discover}/>
+</>
+</Router>
   );
 }
 
